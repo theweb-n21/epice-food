9667dd9d88a25a56ac7da7740874c16597faafd7
MD.Nazmul islam <82874193+theweb-n21@users.noreply.github.com>
2023-05-14 11:33:11 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function navFunction() {
+    document.addEventListener('scroll',()=>{
+        const nav = document.querySelector('nav');
+    
+        if(window.scrollY > 0){
+            nav.classList.add('scrolled')
+        }
+        else{
+            nav.classList.remove('scrolled')
+        }
+    })
+}
+
+navFunction()
+
